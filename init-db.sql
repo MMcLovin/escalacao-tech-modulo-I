7eb75b7e22eed3a6d2385c0c3063b4336f59c10b
MMcLovin <ChadMusashi@protonmail.com>
2026-07-13 23:12:51 -0300

    chore: o grande reset

--- init-db.sql
+++
@@ -1,7 +0,0 @@
--- Criação das bases segregadas para garantir a decomposição de domínios
-CREATE DATABASE analise_db;
-CREATE DATABASE avaliacao_db;
-
--- Permissões adicionais se necessário
-GRANT ALL PRIVILEGES ON DATABASE analise_db TO "user";
-GRANT ALL PRIVILEGES ON DATABASE avaliacao_db TO "user";
